3d276681f0150069e68b2f10bb674694ba458ba1
Robeeen <robeeen@gmail.com>
2024-05-27 08:20:45 +0600

    oop

--- index.js
+++ index.js
@@ -11,3 +11,35 @@ Lucie.introDuction();
 
 const Recca = new Person('Recca');
 Recca.introDuction();
+
+//onject prototypes
+
+const myObject = {
+    city: 'Dhaka',
+    greet(){
+        console.log(`greetings from ${this.city}!`);
+    }
+}
+
+myObject.greet();
+
+class Shape{
+    name; sides; sideLength;
+
+    constructor(name, sides, sideLength){
+        this.name = name;
+        this.sides = sides;
+        this.sideLength = sideLength;
+    }
+
+    calcPerimeter(){
+        console.log(`Length of the ${this.name} is : ${this.sides * this.sideLength}`);
+    }
+
+}
+
+const newShape = new Shape('Square', 4, 5);
+newShape.calcPerimeter();
+
+const anotherShapce = new Shape('Triangle', 3, 3);
+anotherShapce.calcPerimeter();
