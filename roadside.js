d09c1d32d3ffb399d593ed2ff2c377c3983a908b
Robeeen <robeeen@gmail.com>
2024-06-28 11:13:13 +0600

    new

--- roadside.js
+++ roadside.js
@@ -50,7 +50,19 @@ console.timeEnd("Second Call");
 //multiple functions in single const
 
 const calc = {
-    
+    total : 0,
+    add(a){
+        this.total += a;
+        return this;
+    },
+    multiply(a){
+        this.total *= a;
+        return this;
+    },
+    substract(a){
+        this.total -= a;
+        return this;
+    }
 }
 
 const result = calc.add(10).multiply(5).substract(20).add(10);
