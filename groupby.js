ede675af506cde37c5f5be46461e8998f987f8f3
Robeeen <robeeen@gmail.com>
2024-12-12 09:49:01 +0600

    function

--- groupby.js
+++ groupby.js
@@ -10,3 +10,20 @@ const groupbyProducts = Object.groupBy(products, (p) => p.category);
 
 
 console.log(groupbyProducts);
+
+
+//trying to solve a problem.
+
+function setup(){
+    const images = document.querySelector(".image");
+    const insta = document.querySelector(".removes");
+
+    insta.addEventListener("click", (e) => {
+        //images.remove();
+        e.preventDefault();
+        images.remove();
+        //console.log('Test');
+    }) 
+}
+
+setup();
